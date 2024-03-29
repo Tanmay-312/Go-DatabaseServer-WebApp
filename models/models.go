05d101b5f4f8cef85c79e35315531b49ab9ecf64
Tanmay-312 <jain2002tj@gmail.com>
2024-03-29 17:07:38 +0530

    Added files

---
+++ models/models.go
@@ -0,0 +1,12 @@
+package models
+
+type Stock struct {
+	StockID int64  `json:"stockid"`
+	Name    string `json:"name"`
+	Price   int64  `json:"price"`
+	Company string `json:"company"`
+}
+
+func init() {
+
+}
